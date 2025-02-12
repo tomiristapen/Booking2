49ce1d2606689eb4f708bb35185aad9f3c53316d
tomiristapen <tomiristn@gmail.com>
2025-02-12 19:38:52 +0600

    Reinstall dependencies to fix bcrypt issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const client = require('../config/db');
 const secretKey = process.env.JWT_SECRET_KEY;
